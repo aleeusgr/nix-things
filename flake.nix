eb2eab1fe566d7cd9b505063432134882672c497
50^2 <alexeusgr@gmail.com>
2022-11-30 09:48:05 +0200

    add home-manager

--- flake.nix
+++ flake.nix
@@ -2,8 +2,30 @@
  description = "my-system";
  nixConfig.bash-prompt = "\\[\\e[0m\\][\\[\\e[0;2m\\]plutus-testing \\[\\e[0;1m\\]plutus-testing \\[\\e[0;93m\\]\\w\\
 [\\e[0m\\]]\\[\\e[0m\\]$ \\[\\e[0m\\]";
-  # inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
 
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    #nurpkgs.url = github:nix-community/NUR;
+
+    #homeage = {
+    #  url = github:jordanisaacs/homeage;
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};
+
+    #neovim-flake = {
+    #  url = github:gvolpe/neovim-flake;
+    #  # neovim-flake pushes its binaries to the cache using its own nixpkgs version
+    #  # if we instead use ours, we'd be rebuilding all plugins from scratch
+    #  #inputs.nixpkgs.follows = "nixpkgs";
+    #};
+
+  };
   outputs = { self, nixpkgs }: {
 
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
