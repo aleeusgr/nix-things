d6665e57ed155cac8e7886aca8368a5608b7bd55
50^2 <alexeusgr@gmail.com>
2022-12-03 17:14:03 +0200

    restore flake

--- flake.nix
+++ flake.nix
@@ -1,64 +1,55 @@
 {
-  #inputs.nixpkgs-2105.url = "github:nixos/nixpkgs/nixos-21.05";
-  #inputs.nixpkgs-2111.url = "github:nixos/nixpkgs/nixos-21.11";
-  #inputs.nixpkgs-2205.url = "github:nixos/nixpkgs/nixos-22.05";
-  #inputs.unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.master.url = "github:nixos/nixpkgs/master";
-  #inputs.nixpkgs-insync.url = "github:nixos/nixpkgs/bd751508cf67db3b13b03e25eb937854fc92ee30";
+ description = "my-system";
+ nixConfig.bash-prompt = "\\[\\e[0m\\][\\[\\e[0;2m\\]plutus-testing \\[\\e[0;1m\\]plutus-testing \\[\\e[0;93m\\]\\w\\
+[\\e[0m\\]]\\[\\e[0m\\]$ \\[\\e[0m\\]";
 
-  inputs.parts.url = "github:hercules-ci/flake-parts";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
 
-  # The following is required to make flake-parts work.
-  inputs.nixpkgs.follows = "unstable";
-
-  # inputs.nix.url = "github:nixos/nix"; #/caf51729450d4c57d48ddbef8e855e9bf65f8792";
-  # inputs.rnix-lsp.url = "github:nix-community/rnix-lsp/master";
-  # inputs.rnix-lsp.inputs.nixpkgs.follows = "nixpkgs-2111";
-  # inputs.rnix-lsp.inputs.naersk.inputs.nixpkgs.follows = "unstable";
-
-  #inputs.nil.url = "github:oxalica/nil";
-
-  inputs.home-manager.url = "github:nix-community/home-manager";
-  inputs.home-manager.inputs.nixpkgs.follows = "unstable";
-
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  #inputs.emacs.url = "github:nix-community/emacs-overlay";
-  #inputs.emacs.inputs.nixpkgs.follows = "master";
-
-  inputs.nixos-vscode-server.url = "github:mudrii/nixos-vscode-ssh-fix/main";
-
-  #inputs.statix.url = "github:nerdypepper/statix";
-  #inputs.alejandra.url = "github:kamadorueda/alejandra/3.0.0";
-
-  outputs = {
-    self,
-    parts,
-    ...
-  } @ inputs:
-    parts.lib.mkFlake {inherit self;} {
-      systems = ["x86_64-linux"];
-
-      imports = [
-        ./parts/auxiliary.nix
-        ./parts/home_configs.nix
-        ./parts/system_configs.nix
-
-        ./nixos/configurations
-        ./home/configurations
-      ];
-
-      flake = {
-        nixosModules = import ./nixos/modules inputs;
-
-        homeModules = import ./home/modules inputs;
-
-        #mixedModules = import ./mixed inputs;
-
-        packages.x86_64-linux = import ./packages inputs "x86_64-linux";
-        #packages.aarch64-darwin = import ./packages inputs "aarch64-darwin";
-
-        checks.x86_64-linux = import ./checks inputs;
-      };
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    #nurpkgs.url = github:nix-community/NUR;
+
+    #homeage = {
+    #  url = github:jordanisaacs/homeage;
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};
+
+    #neovim-flake = {
+    #  url = github:gvolpe/neovim-flake;
+    #  # neovim-flake pushes its binaries to the cache using its own nixpkgs version
+    #  # if we instead use ours, we'd be rebuilding all plugins from scratch
+    #  #inputs.nixpkgs.follows = "nixpkgs";
+    #};
+
+  };
+  outputs = { self, nixpkgs, home-manager}: {
+
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        [ ({ pkgs, ... }: {
+
+            # Let 'nixos-version --json' know about the Git revision
+            # of this flake.
+            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+
+            # Network configuration.
+            # networking.useDHCP = false;
+            # networking.firewall.allowedTCPPorts = [ 80 ];
+
+            # Enable a web server.
+            # services.httpd = {
+            #  enable = true;
+            #  adminAddr = "morty@example.org";
+            #};
+          })
+          ./configuration.nix
+        ];
+    };
+
+  };
 }
