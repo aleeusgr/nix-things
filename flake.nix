77f598dd96da0a6f28ff7a5cb3e5f7e5613a808d
50^2 <alexeusgr@gmail.com>
2022-12-04 18:05:40 +0200

    clean flake

--- flake.nix
+++ flake.nix
@@ -4,30 +4,32 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
-    nurpkgs = {
-      url = github:nix-community/NUR;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    #nurpkgs = {
+    #  url = github:nix-community/NUR;
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};
 
     home-manager = {
       url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    tex2nix = {
-      url = github:Mic92/tex2nix/4b17bc0;
-      inputs.utils.follows = "nixpkgs";
-    };
+    #tex2nix = {
+    #  url = github:Mic92/tex2nix/4b17bc0;
+    #  inputs.utils.follows = "nixpkgs";
+    #};
   };
 
-  outputs = inputs @ { self, nixpkgs, nurpkgs, home-manager, tex2nix }:
+  #outputs = inputs @ { self, nixpkgs, nurpkgs, home-manager, tex2nix }:
+  outputs = inputs @ { self, nixpkgs, home-manager, }:
     let
       system = "x86_64-linux";
     in
     {
       homeConfigurations = (
         import ./outputs/home-conf.nix {
-          inherit system nixpkgs nurpkgs home-manager tex2nix;
+          #inherit system nixpkgs nurpkgs home-manager tex2nix;
+          inherit system nixpkgs home-manager ;
         }
       );
 
@@ -38,10 +40,5 @@
         }
       );
 
-      devShell.${system} = (
-        import ./outputs/installation.nix {
-          inherit system nixpkgs;
-        }
-      );
     };
 }
