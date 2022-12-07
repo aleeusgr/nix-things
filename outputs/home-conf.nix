c947668082552a95a107803032292e84ddf653f7
50^2 <alexeusgr@gmail.com>
2022-12-07 08:18:14 +0200

    home

---
+++ outputs/home-conf.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+in
+{
+  imports = [
+    (import "${home-manager}/nixos")
+  ];
+
+  home-manager.users.my_username = {
+    /* Here goes your home-manager config, eg home.packages = [ pkgs.foo ]; */
+  };
+}
