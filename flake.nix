d7ef3abdd236bcb1b8101aeab83718dd763d5bef
50^2 <alexeusgr@gmail.com>
2022-12-15 09:54:29 +0200

    name for system

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
-      hostname = nixpkgs.lib.nixosSystem {
+      m3800 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
