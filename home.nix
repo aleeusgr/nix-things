9fe9c206ea04e0750e52ad55fa84f0b4f7a1508c
50^2 <alexeusgr@gmail.com>
2022-12-15 08:57:21 +0200

    home.nix from home manager manual

--- home.nix
+++ home.nix
@@ -1,14 +1,21 @@
 { config, pkgs, ... }:
-let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
-in
-{
-  imports = [
-    (import "${home-manager}/nixos")
-  ];
 
-  home-manager.users.alex = {
-    /* Here goes your home-manager config, eg home.packages = [ pkgs.foo ]; */
-    home.packages = [ pkgs.tldr ];
-  };
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "alex";
+  home.homeDirectory = "/home/alex";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 }
