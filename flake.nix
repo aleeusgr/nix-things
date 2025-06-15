2f5e99e4de9a1d48245e9b08ed2d2085d99f6485
Alex <alexeusgr@gmail.com>
2025-06-15 08:48:52 +0700

    refactor flake

--- flake.nix
+++ flake.nix
@@ -3,21 +3,26 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       vivo = nixpkgs.lib.nixosSystem {
+
         system = "x86_64-linux";
+        
         modules = [
           ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.alex = import ./home.nix;
+          home-manager.nixosModules.home-manager {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.alex = import ./home.nix; #
+            };
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
